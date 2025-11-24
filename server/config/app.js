16125bd798ae5bac6bdffa4c48a801b596ca88cf
rizwanziaee-pixel <rizwan.ziaee@ontariotechu.net>
2025-11-23 19:03:23 -0500

    Update homepage and add custom styling

--- server/config/app.js
+++ server/config/app.js
@@ -1,39 +1,66 @@
-
+// server/config/app.js
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Import routers
+var indexRouter = require('../routes/index');
+var expenseRouter = require('../routes/expense');
 
 var app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+// View engine setup
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
+// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Static files - fix the path to go up two directories to reach public folder
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use('/bootstrap', express.static(path.join(__dirname, '../../node_modules/bootstrap/dist')));
+app.use('/@fortawesome', express.static(path.join(__dirname, '../../node_modules/@fortawesome')));
+app.use('/jquery', express.static(path.join(__dirname, '../../node_modules/jquery/dist')));
+
+// Route configuration
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/expenses', expenseRouter);
 
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
+// Error handler
 app.use(function(err, req, res, next) {
+  // Set locals, only providing error in development
   res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
   
+  // Render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { title: 'Error' });
 });
 
-const connectDB = require('./db');
-connectDB();
+// Database connection
+const mongoose = require('mongoose');
+const dbConfig = require('./db');
+
+// Connect to MongoDB
+mongoose.connect(dbConfig.URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection Error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB at: ' + dbConfig.URI);
+});
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,39 +1,19 @@
+// server/routes/index.js
 var express = require('express');
 var router = express.Router();
 
-
-
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home',
-    displayName: req.user?req.user.displayName:""
-   });
+  res.render('index', { 
+    title: 'Home'
+  });
 });
 
-/* GET home page. */
+/* GET home page */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home',displayName: req.user?req.user.displayName:"" });
+  res.render('index', { 
+    title: 'Home'
+  });
 });
-
-/* GET About page. */
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About us',displayName: req.user?req.user.displayName:"" });
-});
-
-/* GET products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products',displayName: req.user?req.user.displayName:"" });
-});
-
-/* GET Services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services',displayName: req.user?req.user.displayName:"" });
-});
-
-/* GET home page. */
-router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact us',displayName: req.user?req.user.displayName:"" });
-});
-
 
 module.exports = router;
